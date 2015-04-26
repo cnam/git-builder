7d7a732e35310f6ac4114e3b10345e2776338a62
Vasiliy Tyubek <v.tyubek@gmail.com>
2015-04-26 15:50:05 +0300

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-git clone -b $REPO_REF https://$GITLAB_LOGIN:$GITLAB_PASSWORD@$REPO_URL .
+if [ -d ".git" ]; then
+    git pull
+else
+    git clone -b $REPO_REF https://$GITLAB_LOGIN:$GITLAB_PASSWORD@$REPO_URL .
+fi
 
